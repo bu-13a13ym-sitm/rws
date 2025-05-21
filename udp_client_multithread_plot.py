8dc4970efcb94cc4c0a59f6a9b3f467e186ccce5
bu_13a13ym_sitm <jin0722.nzw@gmail.com>
2025-05-21 20:24:38 +0900

    modification to record accel sensor values

--- udp_client_multithread_plot.py
+++ udp_client_multithread_plot.py
@@ -87,6 +87,7 @@ if __name__ == '__main__':
             if th.received:
                 sum_accel += th.get_data()[0]
                 count += 1
+            time.sleep(0.1)
         default_accel = sum_accel / count
         print("initializing completed.")
         time.sleep(3)
