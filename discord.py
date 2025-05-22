bd93f9bf88d98707cd1cdb9085ae56750190348e
bu_13a13ym_sitm <jin0722.nzw@gmail.com>
2025-05-22 18:51:43 +0900

    add discord connection

--- discord.py
+++ discord.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
     key_width = 1.0
 
     try:
-        driver_path = r".\rws\chromedriver-win64\chromedriver.exe"
+        driver_pass = os.path.expanduser("~/exp2025-05-19/Expdir/chromedriver")
         options = webdriver.ChromeOptions()
         options.add_argument("--user-agent={}".format(user_agent))
         options.add_argument("--no-sandbox")
